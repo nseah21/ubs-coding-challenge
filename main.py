7413c045406d91d269ff57b80f877394f08ced5b
nseah21 <nseah21@gmail.com>
2024-08-31 22:56:19 +0800

    Move School and Student classes into main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import json
 import re
 from collections import defaultdict
 import math
-from School import School
-from Student import Student
 
 
 def main():
@@ -89,6 +87,33 @@ def is_alumni(student, school):
 
 def is_volunteer(student, school):
     return student.volunteer and school.name == student.volunteer
+
+
+class Student:
+    def __init__(self, id, homeLocation, alumni="", volunteer=""):
+        self.id = id
+        self.homeLocation = homeLocation
+        self.alumni = alumni
+        self.volunteer = volunteer
+
+    def __repr__(self):
+        is_alumni = (
+            "Is alumni of " if self.alumni else "Is not alumni of "
+        ) + self.alumni
+        is_volunteer = (
+            "Is volunteer of " if self.volunteer else "Is not volunteer of "
+        ) + self.volunteer
+        return f"Student {self.id}, at home location {self.homeLocation}. {is_alumni}. {is_volunteer}"
+
+
+class School:
+    def __init__(self, name, location, maxAllocation):
+        self.name = name
+        self.location = location
+        self.maxAllocation = maxAllocation
+
+    def __repr__(self):
+        return f"School {self.name}, at location {self.location}, with max allocation {self.maxAllocation}"
 
 
 if __name__ == "__main__":
