8326cfed6be2df70b2ce925d7d602e1adcc04b9e
nseah21 <nseah21@gmail.com>
2024-08-31 22:53:59 +0800

    Remove unused comments

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
 import json
 import re
-from collections import defaultdict 
+from collections import defaultdict
 import math
 from School import School
 from Student import Student
 
 
 def main():
-    # schoolA = School("School A", [1, 3], 1)
-    # schoolB = School("School B", [2, 4], 1)
-
-    # student1 = Student(1, [0, 0], "School B", "School B")
-    # student2 = Student(2, [5, 5], "School A", "School B")
-    # student3 = Student(3, [2, 5], "School A")
-    # student3 = Student(4, [2, 4], "School B")
-
     schools, students = read_json("input.json")
-
-    # schools = [schoolA, schoolB]
-    # students = [student2, student1, student3]
-
     students.sort(key=lambda x: x.id)
-
-    for school in schools:
-        # print(school.name)
-        for student in students:
-            pass
-        # print(f"Student {student.id}")
-        # print(normalise_school_distance(compute_school_distance(school.location, student.homeLocation)))
-        # print(is_alumni(student, school))
-
     res = []
 
     for school in schools:
@@ -40,19 +19,12 @@ def main():
         for _ in range(school.maxAllocation):
             curr[school.name].append(students.pop().id)
         res.append(curr)
-            # print(list(map(lambda x: x.id, students)))
-        # resort the queue
-        # remove first element from the queue
-    
-        with open("output.json", "w") as fp: 
-            # Dump JSON with separators and a 2-space indent 
-            json_string = json.dumps(res, indent=2, separators=(',', ': ')) 
- 
-            # Post-process the string to move lists onto the same line as their keys 
-            json_string = json_string.replace('\n      ', '').replace('\n    ]', ']') 
- 
-            fp.write(json_string)
 
+        with open("output.json", "w") as fp:
+            json_string = json.dumps(res, indent=2, separators=(",", ": "))
+            json_string = json_string.replace("\n      ", "").replace("\n    ]", "]")
+
+            fp.write(json_string)
 
 
 def allocate_students_based_on_weightage(school, student):
@@ -66,9 +38,6 @@ def allocate_students_based_on_weightage(school, student):
 
     overall_score = 0.5 * distance_score + 0.3 * alumni_score + 0.2 * volunteer_score
 
-    # print()
-    # print(school.name)
-    # print(f"Student{student.id}")
     return overall_score
 
 
@@ -89,7 +58,7 @@ def read_json(file_path):
             all_schools.append(School(**school))
         for student in student_data:
             all_students.append(Student(**student))
-            
+
     return all_schools, all_students
 
 
@@ -123,8 +92,4 @@ def is_volunteer(student, school):
 
 
 if __name__ == "__main__":
-    # input_file = 'input.json'  # You can change the file path if needed
-    # schools, students = read_json(input_file)
-    # print(schools)
-    # print(students)
     main()
